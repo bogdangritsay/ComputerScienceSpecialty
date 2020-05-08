f66d3b4e70f48a9d548b2499354099743d2a6460
Bogdan <elektro739@gmail.com>
2020-05-08 19:26:24 +0300

    Сгенерирован проект Spring MVC. Проинициализирован проект. Добавлено: - необходимые стартовые зависимости; - контроллер для домашней страницы (маппинг "/"); - необходимые каталоги.

---
+++ src/main/java/com/mcsumdu/hritsay/specialty/ComputerScienceSpecialtyApplication.java
@@ -0,0 +1,13 @@
+package com.mcsumdu.hritsay.specialty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerScienceSpecialtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerScienceSpecialtyApplication.class, args);
+    }
+
+}
